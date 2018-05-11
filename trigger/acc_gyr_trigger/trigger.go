cc74e37393a001d2339a9617da83ffe6eb3f2607
Alan Lee <allee@allee-mbp.apac.tibco.com>
2018-05-11 11:51:32 +0800

    remove unused package

--- trigger/acc_gyr_trigger/trigger.go
+++ trigger/acc_gyr_trigger/trigger.go
@@ -1,34 +1,34 @@
 package acc_gyr_trigger
 
 import (
+	"fmt"
 	"github.com/TIBCOSoftware/flogo-lib/core/action"
 	"github.com/TIBCOSoftware/flogo-lib/core/trigger"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
-        "fmt"
-        "time"
-        "math"
-        "os"
+	"time"
+	//        "math"
+	"github.com/kidoman/embd"
+	_ "github.com/kidoman/embd/host/all"
+	"os"
 	"strconv"
-        "github.com/kidoman/embd"
-        _ "github.com/kidoman/embd/host/all"
-//        "./lsm9ds1"
+	//        "./lsm9ds1"
 )
 
 var log = logger.GetLogger("acc-gyr-trigger")
 
 // MyTriggerFactory My Trigger factory
-type MyTriggerFactory struct{
+type MyTriggerFactory struct {
 	metadata *trigger.Metadata
 }
 
 //NewFactory create a new Trigger factory
 func NewFactory(md *trigger.Metadata) trigger.Factory {
-	return &MyTriggerFactory{metadata:md}
+	return &MyTriggerFactory{metadata: md}
 }
 
 //New Creates a new trigger instance for a given id
 func (t *MyTriggerFactory) New(config *trigger.Config) trigger.Trigger {
-	return &MyTrigger{metadata: t.metadata, config:config}
+	return &MyTrigger{metadata: t.metadata, config: config}
 }
 
 // MyTrigger is a stub for your Trigger implementation
@@ -54,76 +54,75 @@ func (t *MyTrigger) Start() error {
 
 	tmp := t.config.GetSetting("sampleRate")
 	tmpInt, _ := strconv.Atoi(tmp)
-	interval := 1000/tmpInt
+	interval := 1000 / tmpInt
 
 	fmt.Println("interval : %d\n", interval)
-        lsm := NewLSM9DS1(embd.NewI2CBus(1))
-        if (!lsm.DetectIMU()) {
-                os.Exit(1)
-        }
-        lsm.EnableIMU()
+	lsm := NewLSM9DS1(embd.NewI2CBus(1))
+	if !lsm.DetectIMU() {
+		os.Exit(1)
+	}
+	lsm.EnableIMU()
 
-        magRaw := make([]int16, 3)
-        accRaw := make([]int16, 3)
-        gyrRaw := make([]int16, 3)
+	magRaw := make([]int16, 3)
+	accRaw := make([]int16, 3)
+	gyrRaw := make([]int16, 3)
 
-        for {
-                lsm.ReadMAG(magRaw)
-                fmt.Printf("Mag : %v\n", magRaw)
-                lsm.ReadACC(accRaw)
-                fmt.Printf("Acc : %v\n", accRaw)
-                lsm.ReadGYR(gyrRaw)
-                fmt.Printf("Gyr : %v\n", gyrRaw)
+	for {
+		lsm.ReadMAG(magRaw)
+		fmt.Printf("Mag : %v\n", magRaw)
+		lsm.ReadACC(accRaw)
+		fmt.Printf("Acc : %v\n", accRaw)
+		lsm.ReadGYR(gyrRaw)
+		fmt.Printf("Gyr : %v\n", gyrRaw)
 
-                // Init handlers
-                for _, handlerCfg := range t.config.Handlers {
+		// Init handlers
+		for _, handlerCfg := range t.config.Handlers {
 
-                        if handlerIsValid(handlerCfg) {
-                                log.Debugf("Object Detection Trigger: Registering handler for Action Id: [%s]", handlerCfg.ActionId)
-                                fmt.Printf("Object Detection Trigger: Registering handler for Action Id: [%s]", handlerCfg.ActionId)
+			if handlerIsValid(handlerCfg) {
+				log.Debugf("Object Detection Trigger: Registering handler for Action Id: [%s]", handlerCfg.ActionId)
+				fmt.Printf("Object Detection Trigger: Registering handler for Action Id: [%s]", handlerCfg.ActionId)
 
 				t1 := time.Now()
 				t2 := t1.Format("2006-01-02 15:04:05.000000")
 				out := fmt.Sprintf("%s,%d,%d,%d,%d,%d,%d,%d,%d,%d", t2, accRaw[0], accRaw[1], accRaw[2], gyrRaw[0], gyrRaw[1], gyrRaw[2], magRaw[0], magRaw[1], magRaw[2])
-                                data := map[string]interface{}{
-                                        "output": out,
-					"eventTime" : t2,
-                                        "accX":  accRaw[0],
-                                        "accY":  accRaw[1],
-                                        "accZ":  accRaw[2],
-                                        "gyrX":  gyrRaw[0],
-                                        "gyrY":  gyrRaw[1],
-                                        "gyrZ":  gyrRaw[2],
-                                        "magX":  magRaw[0],
-                                        "magY":  magRaw[1],
-                                        "magZ":  magRaw[2],
-                                }
+				data := map[string]interface{}{
+					"output":    out,
+					"eventTime": t2,
+					"accX":      accRaw[0],
+					"accY":      accRaw[1],
+					"accZ":      accRaw[2],
+					"gyrX":      gyrRaw[0],
+					"gyrY":      gyrRaw[1],
+					"gyrZ":      gyrRaw[2],
+					"magX":      magRaw[0],
+					"magY":      magRaw[1],
+					"magZ":      magRaw[2],
+				}
 
-                                //todo handle error
-                                startAttrs, err := t.metadata.OutputsToAttrs(data, false)
-                                if err != nil {
-                                        log.Errorf("After run error' %s'\n", err)
-                                        return err
-                                }
+				//todo handle error
+				startAttrs, err := t.metadata.OutputsToAttrs(data, false)
+				if err != nil {
+					log.Errorf("After run error' %s'\n", err)
+					return err
+				}
 
+				// run next action
+				act := action.Get(handlerCfg.ActionId)
+				ctx := trigger.NewInitialContext(startAttrs, handlerCfg)
+				_, err = t.runner.RunAction(ctx, act, nil)
 
-                                // run next action
-                                act := action.Get(handlerCfg.ActionId)
-                                ctx := trigger.NewInitialContext(startAttrs, handlerCfg)
-                                _ , err = t.runner.RunAction(ctx, act, nil)
+				if err != nil {
+					log.Debugf("Object Detection Trigger Error: %s", err.Error())
+					fmt.Printf("Object Detection Trigger Error: %s", err.Error())
+					return nil
+				}
+			} else {
+				panic(fmt.Sprintf("Invalid handler: %v", handlerCfg))
+			}
+		}
 
-                                if err != nil {
-                                        log.Debugf("Object Detection Trigger Error: %s", err.Error())
-                                        fmt.Printf("Object Detection Trigger Error: %s", err.Error())
-                                        return nil
-                                }
-                        } else {
-                                panic(fmt.Sprintf("Invalid handler: %v", handlerCfg))
-                        }
-                }
-
-                time.Sleep(time.Duration(interval) * time.Millisecond)
-        }
+		time.Sleep(time.Duration(interval) * time.Millisecond)
+	}
 
 	return nil
 }
@@ -135,8 +134,7 @@ func (t *MyTrigger) Stop() error {
 }
 
 func handlerIsValid(handler *trigger.HandlerConfig) bool {
-        //validate path
+	//validate path
 
-        return true
+	return true
 }
-
